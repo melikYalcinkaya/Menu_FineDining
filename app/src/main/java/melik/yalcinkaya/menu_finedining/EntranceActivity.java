30537a533b19b944cbc757d35f3fd28af2c326a4
melikYalcinkaya <melikyalcinkaya55@gmail.com>
2025-03-21 23:38:32 +0300

    fixing constraints

--- app/src/main/java/melik/yalcinkaya/menu_finedining/EntranceActivity.java
+++ app/src/main/java/melik/yalcinkaya/menu_finedining/EntranceActivity.java
@@ -1,7 +1,10 @@
 package melik.yalcinkaya.menu_finedining;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
@@ -21,5 +24,30 @@ public class EntranceActivity extends AppCompatActivity {
         spinner_languages = findViewById(R.id.spinner); // spinner_id'sini tanımlama
         languages_adapter = new ArrayAdapter<String>(context, android.R.layout.simple_spinner_dropdown_item, languages);
         spinner_languages.setAdapter(languages_adapter);
+
+        //spinner_languages.setSelection(1);
+
+
+
+        // Seçim dinleyicisini ekleyelim
+        spinner_languages.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                String selectedLanguage = languages[position];
+
+                if (selectedLanguage.equals("English")) {
+                    // English seçildiğinde MainActivity'ye geç
+                    Intent intent = new Intent(EntranceActivity.this, MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                // Seçim yapılmadığında hiçbir şey yapma
+            }
+        });
+
+
     }
 }
--- app/src/main/java/melik/yalcinkaya/menu_finedining/MainActivity.java
+++ app/src/main/java/melik/yalcinkaya/menu_finedining/MainActivity.java
@@ -1,6 +1,10 @@
 package melik.yalcinkaya.menu_finedining;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +24,15 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // Durum çubuğunu siyah yap
+        Window window = getWindow();
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.setStatusBarColor(Color.BLACK);
+
+        // İkonları beyaz yap
+        window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+
     }
 }
