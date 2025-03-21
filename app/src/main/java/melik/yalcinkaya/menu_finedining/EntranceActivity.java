1889e93ef502a4225c3231aabd5422ed5ac57862
melikYalcinkaya <melikyalcinkaya55@gmail.com>
2025-03-21 17:00:41 +0300

    just started lol

---
+++ app/src/main/java/melik/yalcinkaya/menu_finedining/EntranceActivity.java
@@ -0,0 +1,13 @@
+package melik.yalcinkaya.menu_finedining;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EntranceActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.acitivity_entrance); // entrance_layout.xml'i bağlama
+    }
+}
