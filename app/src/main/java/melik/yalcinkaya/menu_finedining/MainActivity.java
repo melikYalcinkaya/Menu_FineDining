2b6cbf185db729bf58b01a0a73359c67575616dc
NJE\4-110-7 <aaaa>
2025-03-31 15:37:22 +0200

    deleting home

--- app/src/main/java/melik/yalcinkaya/menu_finedining/MainActivity.java
+++ app/src/main/java/melik/yalcinkaya/menu_finedining/MainActivity.java
@@ -53,11 +53,6 @@ public class MainActivity extends AppCompatActivity {
         window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
 
 
-        Toolbar mainToolbar = findViewById(R.id.mainToolbar);
-        setSupportActionBar(mainToolbar);
-
-        setTitle("Home");
-
         NavHostFragment navHostFragment =
                 (NavHostFragment) getSupportFragmentManager()
                         .findFragmentById(R.id.nav_host_fragment);
@@ -68,8 +63,8 @@ public class MainActivity extends AppCompatActivity {
                 R.id.homeFragment, R.id.dishesFragment
         ).build();
 
-        NavigationUI.setupActionBarWithNavController(
+       /* NavigationUI.setupActionBarWithNavController(
                 this, navController, appBarConfiguration);
-
+*/
     }
 }
