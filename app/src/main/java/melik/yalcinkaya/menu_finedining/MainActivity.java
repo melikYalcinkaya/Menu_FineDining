4b067c37aba8b36ebe105f3cbaa410f114056f12
melikYalcinkaya <melikyalcinkaya55@gmail.com>
2025-04-13 23:28:26 +0300

    fixed  the toolbar, pls dont touch

--- app/src/main/java/melik/yalcinkaya/menu_finedining/MainActivity.java
+++ app/src/main/java/melik/yalcinkaya/menu_finedining/MainActivity.java
@@ -53,8 +53,8 @@ public class MainActivity extends AppCompatActivity {
         window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
 
 
-        Toolbar mainToolbar = findViewById(R.id.mainToolbar);
-        setSupportActionBar(mainToolbar);
+//        Toolbar mainToolbar = findViewById(R.id.mainToolbar);
+//        setSupportActionBar(mainToolbar);
 
         setTitle("Home");
 
