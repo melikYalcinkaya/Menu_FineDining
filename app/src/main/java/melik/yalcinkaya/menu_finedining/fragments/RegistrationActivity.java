24badd7fe9dabaf1a8e3c4138b1ee854705b730d
melikYalcinkaya <melikyalcinkaya55@gmail.com>
2025-05-12 02:31:38 +0300

    registration page done with the back end

--- app/src/main/java/melik/yalcinkaya/menu_finedining/fragments/RegistrationActivity.java
+++ app/src/main/java/melik/yalcinkaya/menu_finedining/fragments/RegistrationActivity.java
@@ -9,18 +9,52 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import melik.yalcinkaya.menu_finedining.R;
+import melik.yalcinkaya.menu_finedining.databinding.ActivityRegistrationBinding;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 
 public class RegistrationActivity extends AppCompatActivity {
+
+    private ActivityRegistrationBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_registration);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        binding = ActivityRegistrationBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        DatabaseReference mDatabase;
+// ...
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        binding.registerSaveButton.setOnClickListener(view -> {
+            // Buraya Firebase'e veri yazma işlemi eklenebilir
+            String name = binding.registerNameText.getText().toString();
+            String email = binding.registerEmailText.getText().toString();
+            String password = binding.registerPasswordText.getText().toString();
+            mDatabase.child("users").child(name).child("email").setValue(email)
+                    .addOnSuccessListener(aVoid -> {
+                        Toast.makeText(this, "Kayıt başarılı!", Toast.LENGTH_SHORT).show();
+                    });
+            mDatabase.child("users").child(name).child("password").setValue(password)
+                    .addOnFailureListener(e -> {
+                        Toast.makeText(this, "Kayıt başarısız: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                    });
         });
+
+
+        binding.registerButton.setOnClickListener(view -> {
+            Intent intent = new Intent(RegistrationActivity.this, LoginActivity.class);
+            startActivity(intent);
+        });
+
     }
 }
